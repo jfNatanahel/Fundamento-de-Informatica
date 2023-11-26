1a181d5c5db1e6ca11d96558db7134cc4fd3b8fd
jfNatanahel <natanahel.jf@gmail.com>
2023-11-26 17:07:47 -0300

    revisado

--- Tp2/#12) Dados dos números naturales A y B, mostrar sus divisores comunes.py
+++ Tp2/#12) Dados dos números naturales A y B, mostrar sus divisores comunes.py
@@ -4,4 +4,6 @@ divisor_a=1
 divisor_b=1
 while divisor_a<=a and divisor_b<=b:
     if a%divisor_a==0 and b%divisor_b==0:
-        print("Divisores comunes")
+        print("Divisores comunes",divisor_a)
+    divisor_a+=1
+    divisor_b+=1
---
+++ Tp2/#13)Dados dos numeros naturales A y B mostrar su maximo comun divisor.py
@@ -0,0 +1,11 @@
+a=int(input("Ingresar un numero natural A: "))
+b=int(input("Ingresar un numero natural B: "))
+divisor_a=1
+divisor_b=1
+maximo_comun=0
+while divisor_a<=a and divisor_b<=b:
+    if a%divisor_a==0 and b%divisor_b==0:
+        maximo_comun+=1
+    divisor_a+=1
+    divisor_b+=1
+print("Maximo comun",maximo_comun)
