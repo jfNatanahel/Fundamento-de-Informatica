23c286005608723815858b0fe0e95f86aaf308f5
jfNatanahel <natanahel.jf@gmail.com>
2023-11-26 21:26:30 -0300

    revisado

--- Tp2/#24)Dados 3 numeros naturales, A, B y C mostrar los multiplos de A, menor que B que no sean divisores de C.py
+++ Tp2/#24)Dados 3 numeros naturales, A, B y C mostrar los multiplos de A, menor que B que no sean divisores de C.py
@@ -2,13 +2,13 @@ a=int(input("Ingresar numero natural A: "))
 b=int(input("Ingresar un numero natural B: "))
 c=int(input("Ingresar un numero natural C: "))
 multiplos=1
-divisores=1
 bandera=0
 while bandera==0:
-    if a*multiplos<b and a*multiplos%divisores==0:
-        print(multiplos)
+    multiplos_a=a*multiplos
+    if multiplos_a<b:
+        if c%multiplos_a!=0:
+            print(multiplos_a)
     else:
         bandera=1
     multiplos+=1
-    divisores+=1
     
---
+++ Tp2/#25)Dado un numero natural, calcular su factorial.py
@@ -0,0 +1,8 @@
+x=int(input("Ingresar un numero natural: "))
+factorial=1
+i=1
+while i<=x:
+    factorial=factorial*i
+
+    i+=1
+print(factorial)
