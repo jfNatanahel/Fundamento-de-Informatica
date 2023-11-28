e27b4434046fb92cdda2672a34208ba74ca42688
jfNatanahel <natanahel.jf@gmail.com>
2023-11-27 23:23:11 -0300

    revisado

--- Tp2/TP2-Variables Simples/#24)Dados 3 numeros naturales, A, B y C mostrar los multiplos de A, menor que B que no sean divisores de C.py
+++ Tp2/TP2-Variables Simples/#24)Dados 3 numeros naturales, A, B y C mostrar los multiplos de A, menor que B que no sean divisores de C.py
@@ -4,9 +4,9 @@ c=int(input("Ingresar un numero natural C: "))
 multiplos=1
 bandera=0
 while bandera==0:
-    multiplos_a=a*multiplos
-    if multiplos_a<b:
-        if c%multiplos_a!=0:
+    multiplos_a=a*multiplos #Multiplos de A
+    if multiplos_a<b: #Multiplos de A < B
+        if c%multiplos_a!=0: #C dividido Multiplo de A no es igual a 0. Imprimir
             print(multiplos_a)
     else:
         bandera=1
