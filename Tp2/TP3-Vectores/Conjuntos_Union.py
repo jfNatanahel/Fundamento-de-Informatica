caa69fe34b044c8bcab9dadb2d09813e482e0e41
jfNatanahel <natanahel.jf@gmail.com>
2023-11-29 11:47:12 -0300

    revisado

--- Tp2/TP3-Vectores/Conjuntos_Union.py
+++ Tp2/TP3-Vectores/Conjuntos_Union.py
@@ -1,20 +1,25 @@
+#Modelo
+#1.Tomar los datos del Conjunto A
+#2.Tomar los datos del Conjunto B
+#3.Pasar los datos del conjuto A al Conjunto C.
+#4.Pasar los datos del Conjunto B, que no esten A al conjunto C.
 n=int(input("Ingresar el tamaño del vector A: "))
-v_a=[None]*n
-for i in range (n):
+v_a=[None]*(n+1)
+for i in range (1,n+1,1):
     v_a[i]=int(input("Ingresar elementos del vector A: "))
 m=int(input("Ingresar el tamaño del vector B: "))
-v_b=[None]*m
-for j in range (m):
+v_b=[None]*(m+1)
+for j in range (1,m+1,1):
     v_b[j]=int(input("Ingresar elementos del vector B: "))
 #Pasar los elementos de A para C.
-c=[]*n
-for i in range(n):
+c=[None]*(n+m+1)
+for i in range(1,n+1,1):
     c[i]=v_a[i]
 l=n #cantidad del vector n
 
-for j in range(m):
+for j in range(1,m+1,1):
     bandera=0
-    for i in range(n):
+    for i in range(1,n+1,1):
         if v_a[i]==v_b[j]:
             bandera=1 #Quiere decir que existen repetidos entre A y B
     if bandera==0:
@@ -22,5 +27,5 @@ for j in range(m):
         c[l]=v_b[j]
 
 #Mostrar el Vector union.
-for i in range (l):
+for i in range (1,l+1,1):
     print(c[i])
