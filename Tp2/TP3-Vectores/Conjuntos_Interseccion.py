79099d17a4573cb17d85565f342cd85f757855b4
jfNatanahel <natanahel.jf@gmail.com>
2023-11-29 12:54:38 -0300

    revisado

--- Tp2/TP3-Vectores/Conjuntos_Interseccion.py
+++ Tp2/TP3-Vectores/Conjuntos_Interseccion.py
@@ -3,10 +3,10 @@ n=int(input("Ingresar el tamaño del vector A: "))
 A=[None]*n
 for i in range(n):
     A[i]=int(input("Elementos A: "))
-m=int(input("Ingresar el tamaño del vector A: "))
+m=int(input("Ingresar el tamaño del vector B: "))
 B=[None]*m
 for j in range(m):
-    B[j]=int(input("Elementos A: "))
+    B[j]=int(input("Elementos B: "))
 bandera=0
 c=[None]*(n+m)
 t=0
@@ -17,6 +17,8 @@ for i in range(n):
     if bandera==1:
         t=t+1
         c[t-1]=A[i]
+        bandera=0
+        
 print("Interseccion entre A y B")
 for i in range(t):
     print(c[i])
